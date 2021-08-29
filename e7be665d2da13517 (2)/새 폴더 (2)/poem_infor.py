2088fad9faabbe9c5d6e2407183b9a610920c4cc
jeongminb933 <65400179+jeongminb933@users.noreply.github.com>
2021-08-29 21:48:46 +0900

    Add files via upload

---
+++ e7be665d2da13517 (2)/새 폴더 (2)/poem_infor.py
@@ -0,0 +1,11 @@
+with open("구지가 작자미상(고전시가).txt", "r", encoding='UTF8') as file1:
+    구지가 = file1.read()
+with open("박남수 할머니 꽃시를 받으시다.txt", "r", encoding='UTF8') as file2:
+    꽃시 = file2.read()
+with open("백석 남신의주유동박시봉방.txt", "r", encoding='UTF8') as file3:
+    남신의주유동박시봉방 = file3.read()
+with open("정지용 향수.txt", "r", encoding='UTF8') as file4:
+    향수 = file4.read()
+with open("문법.txt", "r", encoding='UTF8') as file5:
+    문법 = file5.read()
+    문법_list = 문법.split("\n")
